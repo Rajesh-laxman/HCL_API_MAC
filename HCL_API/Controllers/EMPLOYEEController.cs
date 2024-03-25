2d4651636ae36204699b57e55b08554d6c4560b4
rajeshwarl <Rajeshwar.l.505046@gmail.com>
2024-03-25 23:49:23 +0530

    before_repository_changes

--- HCL_API/Controllers/EMPLOYEEController.cs
+++ HCL_API/Controllers/EMPLOYEEController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using HCL_API.DB_CONTEXT;
 using HCL_API.MODEL;
+using HCL_API.MODEL.DTO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,100 +25,102 @@ namespace HCL_API.Controllers
         [HttpGet]
         [Route("Print_all_employees")]
         /*https://localhost:7196/api/EMPLOYEE/Print_all_employees */
-        public IActionResult get_all_emp()
+        public async Task<IActionResult> get_all_emp()
         {
-            var emp_list = hCL_DB_Context.Emp_db_set.ToList();
-            return Ok(emp_list);
+            var emp_DOMAIN_list =await hCL_DB_Context.Emp_db_set.ToListAsync();
+
+            List<EMP_DTO> eMP_DTO_list = new List<EMP_DTO> { };
+            foreach(var x in emp_DOMAIN_list)
+            {
+                eMP_DTO_list.Add(new EMP_DTO()
+                {
+                    E_ID = x.E_ID,
+                    E_Name = x.E_Name,
+                    E_City = x.E_City,
+                    E_Design = x.E_Design
+                });
+
+            }
+            return Ok(eMP_DTO_list);
         }
 
 
         [HttpGet]
         [Route("Print_employee_by_ID")]
         /*https://localhost:7196/api/EMPLOYEE/Print_employee_by_ID?ID=2 */
-        public IActionResult get_emp_by_id (int ID)
+        public async Task<IActionResult> get_emp_by_id (int ID)
         {
-            var emp_detail = hCL_DB_Context.Emp_db_set.FirstOrDefault(x => x.E_ID == ID);
-            if(emp_detail == null)
+            var emp_domain_detail = await hCL_DB_Context.Emp_db_set.FirstOrDefaultAsync(x => x.E_ID == ID);
+            if(emp_domain_detail == null)
             {
                 return NotFound($"THIS ID [{ID}] IS NOT AVAILABLE IN DATA BASE.");
             }
-            return Ok(emp_detail);
+            EMP_DTO eMP_DTO = new EMP_DTO()
+            {
+                E_ID = emp_domain_detail.E_ID,
+                E_City = emp_domain_detail.E_City,
+                E_Design = emp_domain_detail.E_Design,
+                E_Name = emp_domain_detail.E_Name
+            };
+
+
+            return Ok(eMP_DTO);
         }
 
         [HttpPost]
         [Route("Insert_an_employee")]
         /*https://localhost:7196/api/EMPLOYEE/Insert_an_employee */
-        public IActionResult create_an_emp([FromBody] EMPLOYEE e)
+        public async Task<IActionResult> create_an_emp([FromBody] EMP_DTO e)
         {
-            hCL_DB_Context.Emp_db_set.Add(e);
-            hCL_DB_Context.SaveChanges();
-            return Ok(e);
+            EMPLOYEE eMPLOYEE = new EMPLOYEE()
+            {
+                E_Name = e.E_Name,
+                E_Design = e.E_Design,
+                E_City = e.E_City
+            };
+
+            await hCL_DB_Context.Emp_db_set.AddAsync(eMPLOYEE);
+            await hCL_DB_Context.SaveChangesAsync();
+            return Ok(eMPLOYEE);
         }
 
         [HttpPut]
         [Route("Update_an_employee")]
         /*https://localhost:7196/api/EMPLOYEE/Update_an_employee  */
-        public IActionResult update_an_emp([FromBody] EMPLOYEE e)
+        public async Task<IActionResult> update_an_emp([FromBody] EMP_DTO e)
         {
-            hCL_DB_Context.Emp_db_set.Update(e);
-            hCL_DB_Context.SaveChanges();
-            return Ok(e);
+            EMPLOYEE eMPLOYEE = new EMPLOYEE
+            {
+                E_ID = e.E_ID,
+                E_City = e.E_City,
+                E_Design = e.E_Design,
+                E_Name = e.E_Name
+            };
+
+             hCL_DB_Context.Emp_db_set.Update(eMPLOYEE);
+            await hCL_DB_Context.SaveChangesAsync();
+            return Ok(eMPLOYEE);
         }
 
         [HttpDelete]
         [Route("Delete_an_employee/{ID}")]
         /* https://localhost:7196/api/EMPLOYEE/Delete_an_employee/10 */
-        public IActionResult delete_an_emp([FromRoute] int ID)
+        public async Task<IActionResult> delete_an_emp([FromRoute] int ID)
         {
             var emp_details = hCL_DB_Context.Emp_db_set.FirstOrDefault(x => x.E_ID == ID);
             if (emp_details == null)
             {
                 return NotFound($"THIS ID [{ID}] IS NOT AVAILABLE IN DATA BASE.");
             }
-            hCL_DB_Context.Emp_db_set.Remove(emp_details);
-            hCL_DB_Context.SaveChanges();
+             hCL_DB_Context.Emp_db_set.Remove(emp_details);
+            await hCL_DB_Context.SaveChangesAsync();
+
+
+//create automapper to convert domain model to dtos then pass this dtos
             return Ok(emp_details);
         }
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //    [HttpGet]  //https://localhost:7083/STUDENT/Display_Students
-        //    [Route("Display_Students")]
-        //    public IActionResult get_students()
-        //    {
-        //        //var student_details = ._STUDENTS.ToList();
-        //        return Ok("summa summa");
-        //    }
-
-        //}
     }
 
---
+++ HCL_API/MODEL/DTO/EMP_DTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HCL_API.MODEL.DTO
+{
+	public class EMP_DTO
+	{
+		public EMP_DTO()
+		{
+		}
+        public int E_ID { get; set; }
+        public string E_Name { get; set; } = null!;
+        public string? E_City { get; set; }
+        public string E_Design { get; set; } = null!;
+    }
+}
+
